9b9c304ef048a8d6e9fa7c0709923b3bb49368a5
e1qa <e1qa@iMac-5.local>
2020-06-29 13:33:10 +0800

    update test case for pt

--- cypress/integration/e2e/progressTest_spec.js
+++ cypress/integration/e2e/progressTest_spec.js
@@ -22,18 +22,25 @@ describe('EF portal login page custom commands', function () {
         cy.checkPtUi()
     })
 
-    it.only('check progress test status', function () {
-        
-        cy.then(() =>{
-            debugger
-        var units = ['Unit 1','Unit 2','Unit 4']
+    it('check progress test status', function () {
         cy.enterProgressTest()
-        cy.checkPtUi()
-            for (const unit of units) {
-                cy.xpath("//div[text()='Unit 1']/parent::div/parent::div/div[2]//div[text()]").should('have.text', expectedProgressTestStatus[unit])
-            }
-        })
-        
+        cy.contains("View Result").should('have.text',"View Result")
+        cy.contains("Take Test").should('have.text',"Take Test")
+        cy.contains("You can take the test now.").should('have.text',"You can take the test now.")
+        cy.contains("Waiting for your score.").should('have.text',"Waiting for your score.")
+    })
+
+    it('check progress test result', function () {
+        cy.enterProgressTest()
+        cy.xpath("//div[text()='Waiting for your score.']/parent::div/parent::div/div[2]//a").click()
+        cy.contains("Your total score will be available after your writing and speaking assessment.").should('have.text',"Your total score will be available after your writing and speaking assessment.")
+        cy.contains("Grammar").should('contain',"Grammar")
+        cy.contains("Vocabulary").should('contain',"Vocabulary")
+        cy.contains("Listening").should('contain',"Listening")
+        cy.contains("Reading").should('contain',"Reading")
+        cy.contains("Writing").should('have.text',"Writing")
+        cy.contains("Speaking").should('have.text',"Speaking")
+        cy.xpath("//span[text()='Waiting']/parent::div/preceding-sibling::div").should('contain',"Writing").should('contain',"Speaking")
     })
 })
 
