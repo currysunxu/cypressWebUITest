d8cc6f5ffbe7607d78e83e6eea6bbbdf938eac02
e1qa <e1qa@iMac-5.local>
2020-06-29 13:34:52 +0800

    update test case for pt

--- cypress/integration/e2e/progressTest_spec.js
+++ cypress/integration/e2e/progressTest_spec.js
@@ -2,14 +2,7 @@ require('cypress-xpath')
 describe('EF portal login page custom commands', function () {
 
     const expectedResult = 'Progress Test'
-
-    const expectedProgressTestStatus = {
-        "Unit 1":{"View Result":"Waiting for your score."},
-        "Unit 2":{"View Result":"33%"},
-        "Unit 4":{"Take Test":"You can take the test now."}
-    }
     
-
     beforeEach(function setUser() {
         // var hfv2 = user.filter(function (e) { return e.type == "high_flyer_v2";})[0]
         cy.loginPortalByUI(Cypress.env('username'), Cypress.env('password'))
