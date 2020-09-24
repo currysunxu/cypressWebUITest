ba95774d9d1ee59b66227fb169e4edebeaa5e1c2
mac <1234>
2020-09-24 18:59:57 +0800

    fix error

--- cypress/integration/e2e/progressTest_spec.js
+++ cypress/integration/e2e/progressTest_spec.js
@@ -36,8 +36,8 @@ describe('EF portal login page custom commands', function () {
 
     it('check progress test result', function () {
         cy.enterProgressTest()
-        cy.get('.test-item__link-2wkizOcLJl').first().click()
-        cy.get('.test-details__result-description-SeMf0Sra5l').should('have.text', '你将在写作及口语测试评分完成后获得本单元测试总分。')
+        cy.get('[class^=test-item__link]').first().click()
+        cy.get('[class^=test-details__result-description]').should('have.text', '你将在写作及口语测试评分完成后获得本单元测试总分。')
 
         cy.contains("Grammar").should('be.visible')
         cy.contains("Vocabulary").should('be.visible')
@@ -46,13 +46,13 @@ describe('EF portal login page custom commands', function () {
         cy.contains("Writing").should('be.visible')
         cy.contains("Speaking").should('be.visible')
         cy.contains("Grammar").click()
-        cy.get('.txt-1hvfi9ZpL9').first().should('have.text', '上一题')
-        cy.get('.txt-1hvfi9ZpL9').last().should('have.text', '下一题')
-        cy.get('.review-header__right-2Ewo_FSHUc > span').should('have.text', '退出')
-        cy.get('.navigation-bar__icon-menu-21c94dPpgj').click()
+        cy.get('[class^=btn-pre] > span').should('have.text', '上一题')
+        cy.get('[class^=navigation-bar__next]  > [class^= btn] > span').last().should('have.text', '下一题')
+        cy.get('[class^=review-header] > span').should('have.text', '退出')
+        cy.get('[class^=navigation-bar__icon-menu]').click()
         cy.get('.rc-dialog-mask').should('not.have.class', 'rc-dialog-mask-hidden')
         cy.get('.rc-dialog-title').should('have.text', '答题回顾')
-        cy.get('.Button-sc-8m3ouq-0').click()
+        cy.get('[class^=Button]').click()
         cy.get('.rc-dialog-mask').should('have.class', 'rc-dialog-mask-hidden')
     })
 
