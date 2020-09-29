7f99a7fa659956725651e80c78e01afb2db41ebc
Curry Sun <curry.sun@ef.com>
2020-09-29 17:42:13 +0800

    1. update login locator 2. update live_sg.json 3. update live_sg in JenkinsfIle 4. seperate SS,HFV2,HFV3,TB,FR cases in traditional_spec.js

--- cypress/integration/e2e/portal_login_spec.js
+++ cypress/integration/e2e/portal_login_spec.js
@@ -18,44 +18,6 @@ const checkStoryTellerUI = () => {
   cy.get('.ef-container').first().should('have.text', 'Storytellers')
 }
 
-
-describe('Login Portal and verify portal homepage', function () {
-
-  it("Given a small_star_v3 User and Login Then He Can Study Course From Portal", () => {
-    cy.portalLogin('small_star_v3')
-    cy.checkPortalPageLoaded()
-    cy.smallStarPortalCheck()
-  })
-
-  it("Given a High Flyer V2 User and Login Then He Can Study Course From Portal", () => {
-    cy.portalLogin('high_flyer_v2')
-    cy.checkPortalPageLoaded()
-    cy.window_open("sp-banner-animation__btn", "class");
-    cy.openNewWindowByLocalStorage('hf_study_url', 'version1')
-  })
-
-  it("Given a High Flyer V3 User and Login Then He Can Study Course From Portal", () => {
-    cy.portalLogin('high_flyer_v3')
-    cy.checkPortalPageLoaded()
-    cy.window_open("sp-banner-animation__btn", "class");
-    cy.openNewWindowByLocalStorage('hf_study_url', 'version1')
-  })
-
-  it("Given a Traiblazer V3 User and Login Then He Can Study Course From Portal", () => {
-    cy.portalLogin('traiblazer_v3')
-    cy.checkPortalPageLoaded()
-    cy.window_open("sp-banner-animation__btn", "class");
-    cy.openNewWindowByLocalStorage('tb_study_url', 'version3')
-  })
-
-  it("Given a Front Runner User and Login Then He Can Study Course From Portal", () => {
-    cy.portalLogin('front_runner')
-    cy.checkPortalPageLoaded()
-    cy.window_open("sp-banner-animation__btn", "class");
-    cy.openNewWindowByLocalStorage('fr_study_url', 'version1')
-  })
-})
-
 describe('Verify each product card', function () {
   this.beforeEach(() => {
     cy.portalLogin('username')
